3d0072e0ae85e7aca59961cf17944d6ec8d1373a
DavWils <dkwilsonjr03@gmail.com>
2026-01-30 21:54:09 -0800

    Updated item resource
    Updated the item resource to have can_purchase bool

--- scripts/resources/items/itemresource.gd
+++ scripts/resources/items/itemresource.gd
@@ -4,9 +4,11 @@ class_name ItemResource
 
 ## The name of the item.
 @export var item_name: String
-## The cost of this item. If item is -1, not buyable.
-@export var item_cost: int = -1
+## The point value of this item.
+@export var item_cost: int = 0
 ## The mesh used for this item.
 @export var item_mesh: Mesh
 ## The type of collision shape this item will use as a pickup.
 @export var pickup_collision_shape: Shape3D
+## Whether or not this item can be bought in the store.
+@export var can_purchase: bool = item_cost>0
