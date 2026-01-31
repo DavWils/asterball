c1c3a4d489f8629657570281450163c840f3eb96
DavWils <dkwilsonjr03@gmail.com>
2026-01-30 18:37:09 -0800

    Basic item scripts
    Added basic scripts/resources for items and equipment (held items)

---
+++ scripts/resources/items/item.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+## The name of the item.
+@export var item_name: String
+## The cost of this item. If item is -1, not buyable.
+@export var item_cost: int = -1
+## The mesh used for this item.
+@export var item_mesh: Mesh
