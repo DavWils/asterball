12d6ea3c72c8a9936a95bfe8eff7b8578bc14fb0
DavWils <dkwilsonjr03@gmail.com>
2026-02-18 17:38:28 -0800

    Warp Sphere logic
    Warp sphere teleports the thrower to the destination it lands at.

--- scripts/level/projectiles/warp_sphere.gd
+++ scripts/level/projectiles/warp_sphere.gd
@@ -2,4 +2,7 @@ extends Projectile
 
 func projectile_collide(body: Node3D):
 	super.projectile_collide(body)
-	wielder
+	if network_manager.is_host() or thrower_id == network_manager.player_id:
+		throwing_character.position = self.position + Vector3.UP
+		if network_manager.is_host():
+			despawn_projectile()
