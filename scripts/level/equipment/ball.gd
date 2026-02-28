c2a8183db017d22309179eff798bf976c940795b
DavWils <dkwilsonjr03@gmail.com>
2026-02-27 22:23:05 -0800

    Score on pickup
    Allowed ball to result in a score when picked up inside of a score zone.

--- scripts/level/equipment/ball.gd
+++ scripts/level/equipment/ball.gd
@@ -6,6 +6,9 @@ func _ready() -> void:
 	super._ready()
 	var wielder_overlap_area: Area3D = wielder.get_node("OverlapArea3D")
 	wielder_overlap_area.area_entered.connect(_on_area_entered)
+	for body in wielder_overlap_area.get_overlapping_areas():
+		if body is ScoreZone:
+			overlap_score_zone(body)
 
 func _on_area_entered(body: Node3D):
 	if network_manager.is_host():
@@ -13,7 +16,12 @@ func _on_area_entered(body: Node3D):
 		if not level.match_state.state_of_match == level.match_state.StateOfMatch.MATCH: return
 		
 		if body is ScoreZone:
-			if body.owning_team != wielder.get_player_team_id():
-				print("Score!")
-				level = get_tree().current_scene.get_node("Level")
-				level.match_director.score(wielder)
+			overlap_score_zone(body)
+
+
+## Called when player overlaps with a scorezone
+func overlap_score_zone(body: ScoreZone) -> void:
+	if body.owning_team != wielder.get_player_team_id():
+		print("Score!")
+		level = get_tree().current_scene.get_node("Level")
+		level.match_director.score(wielder)
