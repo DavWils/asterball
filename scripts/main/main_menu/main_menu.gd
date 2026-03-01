5d5da59a405b8acddaff86d89623064c3c7eb423
DavWils <dkwilsonjr03@gmail.com>
2026-02-28 22:31:24 -0800

    Main Menu Stars
    Added stars to background of main menu, and made camera rotation rate an export value.

--- scripts/main/main_menu/main_menu.gd
+++ scripts/main/main_menu/main_menu.gd
@@ -1,7 +1,7 @@
 extends Node3D
 
 ## The time it takes for the camera to make a full rotation
-const CAMERA_ROT_RATE: float = -30.0
+@export var camera_rot_rate: float = -30.0
 
 func _process(delta: float) -> void:
-	$Camera3D.rotate(Vector3(0,1,0), delta*(360.0/CAMERA_ROT_RATE) * (PI/180))
+	$Camera3D.rotate(Vector3(0,1,0), delta*(360.0/camera_rot_rate) * (PI/180))
