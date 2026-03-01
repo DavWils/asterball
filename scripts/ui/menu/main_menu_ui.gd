396dce09247ce747f8b931535ebf323338609a4d
DavWils <dkwilsonjr03@gmail.com>
2026-02-28 22:46:22 -0800

    Main Menu Buttons
    Added more buttons to main menu, including a functional quit button.

--- scripts/ui/menu/main_menu_ui.gd
+++ scripts/ui/menu/main_menu_ui.gd
@@ -7,6 +7,9 @@ extends Control
 func _ready() -> void:
 	$MenuButtonsContainer/HostButton.pressed.connect(_on_host_button_pressed)
 	$MenuButtonsContainer/QuickMatchButton.pressed.connect(_on_quick_match_button_pressed)
+	$MenuButtonsContainer/OptionsButton.pressed.connect(_on_options_button_pressed)
+	$MenuButtonsContainer/CreditsButton.pressed.connect(_on_credits_button_pressed)
+	$MenuButtonsContainer/QuitButton.pressed.connect(_on_quit_button_pressed)
 
 
 ## Called when host button is pressed.
@@ -16,3 +19,15 @@ func _on_host_button_pressed():
 ## Called when quick match button is pressed.
 func _on_quick_match_button_pressed():
 	main_scene.quick_find_game()
+
+## Called when options button is pressed.
+func _on_options_button_pressed():
+	pass
+
+## Called when credits button is pressed.
+func _on_credits_button_pressed():
+	pass
+
+## Called when quit button is pressed.
+func _on_quit_button_pressed():
+	get_tree().quit()
