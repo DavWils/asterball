05a6a87abb7c47e85a856e1dee03f025a0113f62
DavWils <dkwilsonjr03@gmail.com>
2026-03-01 10:54:04 -0800

    Credits Menu. resolved #23
    Added a credits menu to the main menu.

---
+++ scripts/ui/menu/credits.gd
@@ -0,0 +1,23 @@
+## Script for the credits screen in main menu
+
+extends Control
+
+const CREDITS_FILE_PATH: String = "res://credits.txt"
+
+@onready var main_menu: Control = get_parent().get_parent()
+@onready var text_label: Label = $ScrollContainer/Label
+@onready var return_button: Button = $VBoxContainer/ReturnButton
+
+func _ready():
+	# Load credits text to label.
+	if not FileAccess.file_exists(CREDITS_FILE_PATH):
+		text_label.text = "credits.txt not found."
+	else:
+		var file: FileAccess = FileAccess.open(CREDITS_FILE_PATH, FileAccess.READ)
+		text_label.text = file.get_as_text()
+	
+	# Connect button signal to return to title screen.
+	return_button.pressed.connect(_on_return_button_pressed)
+
+func _on_return_button_pressed() -> void:
+	main_menu.to_title_screen()
