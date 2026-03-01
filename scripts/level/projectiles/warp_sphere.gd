bda27f4967f3cdc9a419024b1c29586e5d18777e
DavWils <dkwilsonjr03@gmail.com>
2026-02-28 18:47:29 -0800

    Warp Sphere logic separation
    Separated warp sphere logic. When hitting a surface, teleport the thrower. If overlapping a player, swap both players.

--- scripts/level/projectiles/warp_sphere.gd
+++ scripts/level/projectiles/warp_sphere.gd
@@ -1,9 +1,26 @@
 extends Projectile
 
-func projectile_collide(body: Node3D):
-	super.projectile_collide(body)
+func character_overlap(character: Character):
+	super.character_overlap(character)
+	# Hit another character, swap places.
 	if throwing_character:
 		if network_manager.is_host() or thrower_id == network_manager.player_id:
-			throwing_character.position = self.position
+			teleport_character(character, throwing_character.position)
+			teleport_character(throwing_character, self.position)
 			if network_manager.is_host():
 				despawn_projectile()
+
+
+func surface_collide(body: Node3D) -> void:
+	super.surface_collide(body)
+	# Hit a surface, teleport character to surface.
+	if throwing_character:
+		if network_manager.is_host() or thrower_id == network_manager.player_id:
+			teleport_character(throwing_character, self.global_position)
+			if network_manager.is_host():
+				despawn_projectile()
+
+
+
+func teleport_character(character: Character, location: Vector3):
+	character.position = location
