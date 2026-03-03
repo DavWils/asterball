1f477268910b64c8dc9068b8a58f95267542d7b6
DavWils <dkwilsonjr03@gmail.com>
2026-03-02 22:49:22 -0800

    Level Resource functions
    Added functions to level resource to get level filename and level scene packed scene.

--- scripts/resources/levels/levelresource.gd
+++ scripts/resources/levels/levelresource.gd
@@ -7,3 +7,11 @@ class_name LevelResource
 
 ## The homefield team of this level.
 @export var home_team: TeamResource
+
+## Returns text name of the level.
+func get_level_filename() -> String:
+	return self.resource_path.get_file().get_basename()
+
+## Returns the scene file of the level.
+func get_level_scene() -> PackedScene:
+	return load("res://scenes/main/levels/" + get_level_filename() + ".tscn")
