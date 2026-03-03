89a67d8b231b64b0f285a69f9ed540a7006be156
DavWils <dkwilsonjr03@gmail.com>
2026-03-03 13:24:58 -0800

    Singular Wind Audio Player
    Moved WindAudioPlayer to the level instead of the character so there is only one.

--- scripts/level/character/wind_audio_player.gd
+++ scripts/level/character/wind_audio_player.gd
@@ -1,23 +1,14 @@
 extends AudioStreamPlayer
 
-@onready var character: Character = self.get_parent()
-
-func _ready() -> void:
-	character.possessed.connect(_on_possessed)
-	character.unpossessed.connect(_on_unpossessed)
+@onready var player_controller: PlayerController = self.get_parent().get_node("PlayerController")
 
 func _process(_delta: float) -> void:
-	if playing:
+	if playing and player_controller.current_character:
+		var character = player_controller.current_character
 		var current_velocity: float = character.get_body_velocity().length()
 		var speed_ratio = max(current_velocity-8.0, 0.0) / 30.0
 		var new_volume = pow(speed_ratio, 2.2)
-		volume_db = lerp(volume_db, linear_to_db(clampf(new_volume * .2, 0.01, 1.0)), 0.4)
-		pitch_scale = clampf(pitch_scale + (current_velocity if randi()%2 else -current_velocity)*0.001, 0.9, 1.3)
-
-func _on_possessed() -> void:
-	play()
-	print("Wind Audio Playing")
-
-func _on_unpossessed() -> void:
-	stop()
-	print("Wind Audio Stopped")
+		volume_db = lerp(volume_db, linear_to_db(clampf(new_volume * .3, 0.01, 1.0)), 0.4)
+		pitch_scale = clampf(pitch_scale + (current_velocity if randi()%2 else -current_velocity)*0.0001, 0.9, 1.3)
+	else:
+		volume_db = linear_to_db(0.001)
