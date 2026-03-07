5344ef22d5df5eccd8a53376f2b0e98956365e11
DavWils <dkwilsonjr03@gmail.com>
2026-03-07 10:21:01 -0800

    Wind Audio
    Changed functions for wind audio player.

--- scripts/level/wind_audio_player.gd
+++ scripts/level/wind_audio_player.gd
@@ -2,13 +2,22 @@ extends AudioStreamPlayer
 
 @onready var player_controller: PlayerController = self.get_parent().get_node("PlayerController")
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
 	if playing and player_controller.current_character:
 		var character = player_controller.current_character
 		var current_velocity: float = character.get_body_velocity().length()
-		var speed_ratio = max(current_velocity-8.0, 0.0) / 30.0
-		var new_volume = pow(speed_ratio, 2.2)
-		volume_db = lerp(volume_db, linear_to_db(clampf(new_volume * .3, 0.01, 1.0)), 0.4)
-		pitch_scale = clampf(pitch_scale + (current_velocity if randi()%2 else -current_velocity)*0.0001, 0.9, 1.3)
+
+		var min_speed := 6.0
+		var max_speed := 24.0
+
+		var speed_ratio := clampf((current_velocity - min_speed) / (max_speed - min_speed), 0.0, 1.0)
+
+		var new_linear := pow(speed_ratio, 1.5) * 0.3
+		var target_db := linear_to_db(clampf(new_linear, 0.01, 1.0))
+
+		volume_db = lerp(volume_db, target_db, 6.0 * delta)
+		pitch_scale = lerp(pitch_scale, 0.9 + (speed_ratio if randi()%2 else -speed_ratio) * 0.4, 5.0 * delta)
+		print(pitch_scale)
+
 	else:
-		volume_db = linear_to_db(0.001)
+		volume_db = lerp(volume_db, linear_to_db(0.001), 6.0 * delta)
