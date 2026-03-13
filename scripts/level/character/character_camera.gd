239c6af6c9dd1b051b8f7cf7407feb1a2784cdc7
DavWils <dkwilsonjr03@gmail.com>
2026-03-13 16:39:21 -0700

    Camera Collision. Resolves #39
    Added camera collision, so that the camera will not go behind surfaces.

--- scripts/level/character/character_camera.gd
+++ scripts/level/character/character_camera.gd
@@ -26,11 +26,51 @@ var shake_offset := Vector3.ZERO
 func get_base_fov() -> float:
 	return 75.0
 
-func _process(_delta: float) -> void:
+## returns the camera offset to lerp to based on the character status. 0 is for the handle and 1 is for the camera.
+func get_camera_offset() -> Array[Vector3]:
+	var handle_offset: Vector3
+	var cam_offset: Vector3
+	
+	# Get the base offsets.
 	if character.is_tackled():
-		self.position = self.position.lerp(Vector3.UP*2, 0.2)
-		camera.position = camera.position.lerp(Vector3.ZERO, 0.2)
-		
+		handle_offset = Vector3(0,2,0)
+		cam_offset = Vector3.ZERO
+	if character.is_aiming():
+		handle_offset = Vector3(0, AIM_HEIGHT, 0)
+		cam_offset = Vector3(AIM_OFFSET, 0, AIM_LENGTH)
+	else:
+		handle_offset = Vector3(0, BASE_HEIGHT, 0)
+		cam_offset = Vector3(0, 0, BASE_LENGTH)
+	
+	# Raycast to find out new cam transforms when blocked.
+	
+	var handle_global = character.global_transform * handle_offset
+	var desired_cam_global = global_transform * (cam_offset)
+	
+	
+	
+	# Raycast
+	var space_state := get_world_3d().direct_space_state
+	var query := PhysicsRayQueryParameters3D.create(handle_global, desired_cam_global, 1)
+	query.exclude = [character]
+	
+	var result = space_state.intersect_ray(query)
+	
+	if result:
+		var new_cam_global = result.position + (handle_global-result.position).normalized()*0.6
+		cam_offset = to_local(new_cam_global)
+
+	return [handle_offset, cam_offset]
+
+
+
+func _process(_delta: float) -> void:
+	var cam_offset := get_camera_offset()
+	self.position = self.position.lerp(cam_offset[0], 0.2)
+	camera.position = camera.position.lerp(cam_offset[1], 0.2)
+	
+	# While tackled, focus on ragdoll.
+	if character.is_tackled():
 		var ragdoll_position = character.ragdoll.get_ragdoll_position()
 		var target_transform = global_transform.looking_at(ragdoll_position, Vector3.UP)
 
@@ -43,20 +83,20 @@ func _process(_delta: float) -> void:
 		
 		# Zoom camera on ragdoll.
 		var ragdoll_distance = global_position.distance_to(ragdoll_position)
-		camera.fov = lerp(camera.fov, get_base_fov() - ((min(ragdoll_distance*1,50.0))), 0.2)
+		var zoom_offset = (min(ragdoll_distance*4,50.0))
+		print(get_base_fov() - zoom_offset)
+		camera.fov = lerp(camera.fov, get_base_fov() - zoom_offset, 0.2)
 	else:
 		self.rotation.x = lerp(self.rotation.x, character.control_pitch, .6)
 		self.rotation.y = lerp(self.rotation.y, 0.0, 0.6)
-		if character.is_aiming():
-			self.position = self.position.lerp(Vector3(0, AIM_HEIGHT, 0), 0.2)
-			camera.position = camera.position.lerp(Vector3(AIM_OFFSET, 0, AIM_LENGTH), 0.2)
-		else:
-			self.position = self.position.lerp(Vector3(0, BASE_HEIGHT, 0), 0.2)
-			camera.position = camera.position.lerp(Vector3(0, 0, BASE_LENGTH), 0.2)
+
+		# While throwing, zoom forward.
 		if character.is_throwing():
 			camera.fov = lerp(camera.fov, get_base_fov() - ((character.throw_component.throw_force / character.throw_component.get_max_throw_force()) * 35), 0.4)
 		else:
 			camera.fov = lerp(camera.fov, get_base_fov(), 0.4)
+	
+	
 	# Camera shake update
 	if shake_time < shake_duration:
 		shake_time += _delta
