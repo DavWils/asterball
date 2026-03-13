fd0def71df7abf2d4a81c9995c9bb6cc44a4ea9d
DavWils <dkwilsonjr03@gmail.com>
2026-03-13 02:43:55 -0700

    Throw Particles
    Added a particle effect that ramps up as character is charging throw.

--- scripts/level/character/throw_component.gd
+++ scripts/level/character/throw_component.gd
@@ -11,6 +11,9 @@ class_name ThrowComponent
 ## Network manager.
 @onready var network_manager: NetworkManager = get_tree().current_scene.network_manager
 
+@onready var throw_particles = $GPUParticles3D
+@onready var throw_sub_particles = $GPUParticles3D/GPUParticles3D2
+
 ## The base maximum throw force the character can throw items with.
 @export var base_max_throw_force: float = 100.0
 ## The amount of throw force to be accumulated in a second.
@@ -26,12 +29,21 @@ var throw_force := 0.0
 ## The max percentage of throw force that is too small to actually throw.
 const MINIMUM_THROW_FORCE := 0.05
 
+func _ready() -> void:
+	if not character.is_node_ready(): await character.ready
+	throw_particles.draw_pass_1.material.emission = character.get_player_team_state().team_resource.primary_color
+	throw_sub_particles.draw_pass_1.material.emission = character.get_player_team_state().team_resource.secondary_color
+
 func _physics_process(delta: float) -> void:
 	# If throwing, accumulate force.
 	if is_throwing:
 		print("T: ", throw_force)
 		throw_force = clampf(throw_force + (get_throw_speed() * delta), 0, get_max_throw_force())
 
+func _process(_delta: float) -> void:
+	if throw_particles.emitting:
+		throw_particles.position = character.current_equipment.global_position
+		throw_particles.amount_ratio = throw_force / get_max_throw_force()
 
 ## Starts aiming with the given item.
 func start_aim() -> void:
@@ -58,13 +70,16 @@ func start_throwing() -> void:
 		print("Charging throw.")
 		throw_force = 0.0
 		is_throwing = true
+		throw_particles.emitting = true
 		if network_manager.is_host():
 			network_manager.send_p2p_packet(0, {"m": network_manager.Message.CHARACTER_THROW_START, "char_id": character.registry_id})
+
 
 func stop_throwing() -> void:
 	if is_throwing:
 		is_throwing = false
 		end_aim()
+		throw_particles.emitting = false
 		if throw_force > get_max_throw_force() * MINIMUM_THROW_FORCE: $ThrowAudioPlayer.play()
 		if network_manager.is_host():
 			network_manager.send_p2p_packet(0, {"m": network_manager.Message.CHARACTER_THROW_END, "char_id": character.registry_id})
