f6f14f6c50d7adf40f53b06285fe67e87480a7eb
David Wilson <dkwilson1@csuchico.edu>
2026-03-17 14:19:48 -0600

    Volume Sliders. Resolves #57
    Added working volume sliders to options menu.

--- scripts/ui/menu/options_menu.gd
+++ scripts/ui/menu/options_menu.gd
@@ -3,13 +3,25 @@ extends Control
 @export var player_ui: PlayerUI
 @export var main_menu_ui: MainMenuUI
 
+@onready var volume_slider_box := $AudioPanel/ScrollContainer/MarginContainer/VolumeSliderContainer
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/ApplyButton.pressed.connect(_on_apply_pressed)
 	$VBoxContainer/ReturnButton.pressed.connect(_on_return_pressed)
+	
+	# Load the volume slider box.
+	var slider_ui := load("res://scenes/ui/menu/options_menu/volume_slider.tscn")
+	for bus in AudioServer.bus_count:
+		var current_slider: Control = slider_ui.instantiate()
+		current_slider.idx = bus
+		volume_slider_box.add_child(current_slider)
 
 func _on_apply_pressed() -> void:
-	pass
+	# Load Volume Sliders.
+	for bus in AudioServer.bus_count:
+		var linear_vol: float = volume_slider_box.get_child(bus).get_linear_vol()
+		AudioServer.set_bus_volume_linear(bus, linear_vol)
 
 func _on_return_pressed() -> void:
 	if player_ui:
---
+++ scripts/ui/menu/options_menu/volume_slider.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var idx: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Label.text = AudioServer.get_bus_name(idx)
+	$HSlider.value = AudioServer.get_bus_volume_linear(idx)
+
+func get_linear_vol() -> float:
+	return $HSlider.value
