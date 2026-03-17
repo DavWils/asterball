e60dc78a90982969511dbc04d8d41554b4de2b7b
David Wilson <dkwilson1@csuchico.edu>
2026-03-17 15:05:17 -0600

    Continuous Run Anim
    Re-Added Continuous Run Animation for Omnistriker, using a timer to prevent footstep sounds from playing too fast.

--- scripts/level/character/footstep_sound_player.gd
+++ scripts/level/character/footstep_sound_player.gd
@@ -1,5 +1,14 @@
 extends AudioStreamPlayer3D
 
+@onready var character: Character = self.get_parent().get_parent()
+
+@export var footstep_wait_time: float
+
+func _ready() -> void:
+	$Timer.wait_time = footstep_wait_time
+
 func play_step_sonud() -> void:
-	pitch_scale = randf_range(0.9,1.2)
-	play()
+	if $Timer.is_stopped() or character.is_charging():
+		pitch_scale = randf_range(0.9,1.2)
+		play()
+		$Timer.start()
