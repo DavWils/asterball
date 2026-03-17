0907992fa96ca13d78d2ee65e594985f2778c9cc
David Wilson <dkwilson1@csuchico.edu>
2026-03-17 15:31:26 -0600

    Volume Slider Text Clarification
    Adjusted the text values of the volume slider labels

--- scripts/ui/menu/options_menu/volume_slider.gd
+++ scripts/ui/menu/options_menu/volume_slider.gd
@@ -4,7 +4,7 @@ var idx: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Label.text = AudioServer.get_bus_name(idx)
+	$Label.text = AudioServer.get_bus_name(idx) + " Volume"
 	$HSlider.value = AudioServer.get_bus_volume_linear(idx)
 
 func get_linear_vol() -> float:
