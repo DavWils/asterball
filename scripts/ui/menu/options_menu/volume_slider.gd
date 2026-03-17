aca423be7f62c60471aa4d24226ed6c0675acb9c
David Wilson <dkwilson1@csuchico.edu>
2026-03-17 15:42:28 -0600

    Options UI Margin Containers
    Added Margin Containers for the Options UI Sliders.

--- scripts/ui/menu/options_menu/volume_slider.gd
+++ scripts/ui/menu/options_menu/volume_slider.gd
@@ -5,7 +5,7 @@ var idx: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Label.text = AudioServer.get_bus_name(idx) + " Volume"
-	$HSlider.value = AudioServer.get_bus_volume_linear(idx)
+	$MarginContainer/HSlider.value = AudioServer.get_bus_volume_linear(idx)
 
 func get_linear_vol() -> float:
-	return $HSlider.value
+	return $MarginContainer/HSlider.value
