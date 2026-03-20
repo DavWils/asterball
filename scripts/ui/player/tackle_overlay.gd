0237be87c14eb8715c9b7f460d7ddee6bc1e1f55
DavWils <dkwilsonjr03@gmail.com>
2026-03-20 15:09:08 -0700

    Recovery Indicator. Resolves #79, Resolves #78

--- scripts/ui/player/tackle_overlay.gd
+++ scripts/ui/player/tackle_overlay.gd
@@ -2,10 +2,20 @@ extends Control
 
 var current_code: Array[int]
 
+## UI player.
+@onready var audio_player: AudioStreamPlayer = get_tree().current_scene.get_node("UIAudioPlayer")
+
 ## Initial x position used for tweening.
 @onready var initial_keybox_pos: Vector2 = $KeyBox.position
 ## The distance to move the widget's position per progress.
 const MOVE_DIST: float = 68.0
+## Time for the move tween.
+const MOVE_TIME: float = 0.2
+## TIme for the vignette effect.
+const VIGNETTE_TIME: float = 0.2
+
+var move_tween: Tween
+
 
 var last_progress: int = -1
 
@@ -37,6 +47,30 @@ func set_recovery_progress(progress: int) -> void:
 			
 	var progress_distance = progress - last_progress
 	last_progress = progress
-	var move_tween := create_tween()
+	if move_tween:
+		move_tween.kill()
+	move_tween = create_tween()
 	$KeyBox.position.x = initial_keybox_pos.x + (progress_distance * MOVE_DIST)
 	move_tween.tween_property($KeyBox, "position", initial_keybox_pos, 0.1)
+	# Show visual indicator based on progress shift.
+	if progress_distance > 0:
+		# Good progress, good indicator.
+		audio_player.pitch_scale = 1.0 + (float(progress) / float(current_code.size()))
+		audio_player.play_recovery_pass()
+		$KeyBox/TextureRect.modulate = Color.GREEN
+		move_tween.parallel().tween_property($KeyBox/TextureRect, "modulate", Color(0,0,0,0), MOVE_TIME)
+		$VignetteRect.color = Color.GREEN
+		move_tween.parallel().tween_property($VignetteRect, "color", Color(0,1,0,0), VIGNETTE_TIME)
+	elif progress_distance == 0:
+		# No Progress
+		audio_player.play_recovery_fail()
+		$VignetteRect.color = Color.GREEN
+		move_tween.parallel().tween_property($VignetteRect, "color", Color(0,1,0,0), VIGNETTE_TIME)
+	else:
+		# Bad progress, bad indicator.
+		audio_player.play_recovery_fail()
+		# Then modulate on the previous node.
+		$KeyBox/TextureRect3.modulate = Color.RED
+		move_tween.parallel().tween_property($KeyBox/TextureRect3, "modulate", Color(1,1,1,0.5), MOVE_TIME)
+		$VignetteRect.color = Color.RED
+		move_tween.parallel().tween_property($VignetteRect, "color", Color(1,0,0,0), VIGNETTE_TIME)
--- scripts/ui/ui_audio_player.gd
+++ scripts/ui/ui_audio_player.gd
@@ -10,24 +10,42 @@ extends AudioStreamPlayer
 @export var purchased_sound: AudioStream
 ## Button sound when failing to purchase an item.
 @export var fail_purchased_sound: AudioStream
+## Button sound when passing a recovery key.
+@export var recovery_pass_sound: AudioStream
+## Button sound when failing a recovery key.
+@export var recovery_fail_sound: AudioStream
 
 
 func play_hover():
 	stream = hover_sound
+	pitch_scale = 1.0
 	play()
 
 func play_pressed():
 	stream = pressed_sound
+	pitch_scale = 1.0
 	play()
 
 func play_pressed_alt():
 	stream = pressed_sound_alt
+	pitch_scale = 1.0
 	play()
 
 func play_purchased():
 	stream = purchased_sound
+	pitch_scale = 1.0
 	play()
 
 func play_fail_purchased():
 	stream = fail_purchased_sound
+	pitch_scale = 1.0
+	play()
+
+func play_recovery_pass():
+	stream = recovery_pass_sound
+	play()
+
+func play_recovery_fail():
+	stream = recovery_fail_sound
+	pitch_scale = 1.0
 	play()
