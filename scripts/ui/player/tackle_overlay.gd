2272df4c49b0a4c9f10d2cfa44e8ac6a9bdd95c5
DavWils <dkwilsonjr03@gmail.com>
2026-03-20 14:04:15 -0700

    Tackle Slide UI. Resolves #80
    Tween property for the tackle UI for a more smooth animation.

--- scripts/ui/player/tackle_overlay.gd
+++ scripts/ui/player/tackle_overlay.gd
@@ -2,6 +2,13 @@ extends Control
 
 var current_code: Array[int]
 
+## Initial x position used for tweening.
+@onready var initial_keybox_pos: Vector2 = $KeyBox.position
+## The distance to move the widget's position per progress.
+const MOVE_DIST: float = 68.0
+
+var last_progress: int = -1
+
 @onready var key_textures: Dictionary[int, Texture2D] = {
 	0: load("res://textures/ui/player/tackle_overlay/tackle_key_up.png"),
 	1: load("res://textures/ui/player/tackle_overlay/tackle_key_left.png"),
@@ -27,3 +34,9 @@ func set_recovery_progress(progress: int) -> void:
 		else:
 			current_child.texture = null
 			current_child.modulate = Color.TRANSPARENT
+			
+	var progress_distance = progress - last_progress
+	last_progress = progress
+	var move_tween := create_tween()
+	$KeyBox.position.x = initial_keybox_pos.x + (progress_distance * MOVE_DIST)
+	move_tween.tween_property($KeyBox, "position", initial_keybox_pos, 0.1)
