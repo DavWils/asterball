61b1ef89156b481b01696848396286cadb65ab11
DavWils <dkwilsonjr03@gmail.com>
2026-03-22 09:59:53 -0700

    Team Column Centering
    Fixed team columns not being centered.

--- scripts/ui/player/match_menu.gd
+++ scripts/ui/player/match_menu.gd
@@ -7,6 +7,7 @@ extends Control
 ## Reference to network manager.
 @onready var network_manager = get_tree().current_scene.network_manager
 
+@export var team_container: HBoxContainer
 
 func _ready() -> void:
 	if network_manager.is_host():
@@ -20,14 +21,14 @@ func _on_game_info_retrieved() -> void:
 	load_team_columns()
 
 func load_team_columns() -> void:
-	for child in $TeamContainer.get_children():
+	for child in team_container.get_children():
 		child.queue_free()
 	
 	var team_column_resource = load("res://scenes/ui/player/match_menu/team_column.tscn")
 	for team_id in match_state.get_team_ids():
 		var team_column = team_column_resource.instantiate()
 		team_column.team_id = team_id
-		$TeamContainer.add_child(team_column)
+		team_container.add_child(team_column)
 
 func _on_time_set(_time: int):
 	$TimerContainer/TimerLabel.text = match_state.get_time_text()
