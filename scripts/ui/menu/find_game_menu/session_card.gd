a719ee1c790f4051ab3e680b836d6ef20f40a3d1
DavWils <dkwilsonjr03@gmail.com>
2026-03-22 14:28:52 -0700

    Session Browser Join Button
    Made the join button in the session browser actually work.

--- scripts/ui/menu/find_game_menu/session_card.gd
+++ scripts/ui/menu/find_game_menu/session_card.gd
@@ -2,6 +2,8 @@ extends Control
 
 var session_id: int
 
+@onready var main_scene: MainScene = get_tree().current_scene
+
 func _ready() -> void:
 	$Panel/MarginContainer/HBoxContainer/VBoxContainer/SessionNameLabel.text = Steam.getLobbyData(session_id, "lobby_name")
 	$Panel/MarginContainer/HBoxContainer/VBoxContainer/SessionContextLabel.text = str(Steam.getNumLobbyMembers(session_id)) + "/" + str(Steam.getLobbyMemberLimit(session_id))
@@ -10,3 +12,8 @@ func _ready() -> void:
 	if session_level_resource:
 		$Panel/MarginContainer/HBoxContainer/SessionLevelThumbnailRect.texture = session_level_resource.thumbnail
 		$Panel/MarginContainer/HBoxContainer/SessionLevelThumbnailRect.tooltip_text = session_level_resource.level_name
+	
+	$Panel/MarginContainer/HBoxContainer/JoinButton.pressed.connect(_on_join_pressed)
+
+func _on_join_pressed() -> void:
+	main_scene.join_game(session_id)
