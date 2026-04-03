33153c5a27dac4f9f3aa57eeba9b2204be9708a7
David Wilson <dkwilson1@csuchico.edu>
2026-04-03 13:49:15 -0700

    Controls Menu Start
    Options UI has more room for controls, using a scroll box. Options UI also gets all input maps and prints their name and key, which will later be used for setting/getting keys.

--- scripts/ui/menu/options_menu.gd
+++ scripts/ui/menu/options_menu.gd
@@ -6,6 +6,8 @@ extends Control
 
 @onready var volume_slider_box := $AudioPanel/ScrollContainer/MarginContainer/VolumeSliderContainer
 
+@onready var controls_box := $ControlsPanel/ScrollContainer/MarginContainer/ControlsBox
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/ApplyButton.pressed.connect(_on_apply_pressed)
@@ -17,6 +19,25 @@ func _ready() -> void:
 		var current_slider: Control = slider_ui.instantiate()
 		current_slider.idx = bus
 		volume_slider_box.add_child(current_slider)
+		
+	# Load the controls box
+	for map in InputMap.get_actions():
+		var input_name := map
+		if input_name.substr(0, 3) == "ui_": continue
+		# Capitalize first letter.
+		input_name = input_name.substr(0, 1).capitalize() + input_name.substr(1)
+		# Replace underscore with space and capitalize subsequent letters.
+		for i in range(0, input_name.length()):
+			var current_char = input_name.substr(i, 1)
+			if current_char == "_":
+				var next_char = input_name.substr(i+1, 1)
+				input_name = input_name.substr(0, i) + " " + next_char.capitalize() + input_name.substr(i+2)
+		print(input_name)
+		var input_map := InputMap.action_get_events(map)[0]
+		if input_map is InputEventKey:
+			print(input_map.as_text_physical_keycode())
+		else:
+			print(input_map.as_text())
 
 func _on_apply_pressed() -> void:
 	var save_dict: Dictionary = {}
